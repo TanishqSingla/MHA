7c5add8545ce92f611acd602a418cf779e3cb779
= <tanishqsingla00@gmail.com>
2020-02-09 15:04:14 +0530

    corrected the script

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-var page = "explore";
 var pageLocation = window.location.href;
 
-if (pageLocation.includes(page)) {
-  document.getElementsByTagName("a")[1].classList.remove("active");
-  document.getElementsByTagName("a")[2].classList.add("active");
+if (!pageLocation.includes("index")) {
+	document.getElementsByTagName("a")[1].classList.remove("active");
+} else if (!pageLocation.includes("explore")) {
+	document.getElementsByTagName("a")[2].classList.remove("active");
+} else if (!pageLocation.includes("community")) {
+	document.getElementsByTagName("a")[3].classList.remove("active");
 }
