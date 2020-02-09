09c4ec8ecb6763f0607e21e8ae2e4c99c4efca89
TanishqSingla <tanishqsingla00@gmail.com>
2020-02-09 14:25:42 +0530

    added explore.html and a exciting feature

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-    
+var page = "explore";
+var pageLocation = window.location.href;
+
+if (pageLocation.includes(page)) {
+  document.getElementsByTagName("a")[1].classList.remove("active");
+  document.getElementsByTagName("a")[2].classList.add("active");
+}
