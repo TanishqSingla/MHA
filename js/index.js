7b0dcf6d61c0ab03302eeec57c9ea3946ca0fb5e
TanishqSingla <tanishqsingla00@gmail.com>
2020-02-14 00:04:18 +0530

    show info toggle done

--- js/index.js
+++ js/index.js
@@ -13,4 +13,12 @@ var box = document.getElementById("box");
 function rotate() {
 	box.classList.toggle("rotate");
 	box.classList.toggle("rotated");
+	var subInfo = document.querySelector(".sub-info");
+	if (box.classList.contains("rotated")) {
+		subInfo.classList.add("show");
+		subInfo.classList.remove("hide");
+	} else {
+		subInfo.classList.add("hide");
+		subInfo.classList.remove("show");
+	}
 }
