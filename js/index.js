6be74ed95e9e721512121d4bd687ee9368d22a32
TanishqSingla <tanishqsingla00@gmail.com>
2020-02-14 23:11:49 +0530

    various changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-var pageLocation = window.location.href;
+var pageLocation = window.location.href; //gets the current url
 
+//the if else below changes the active tab on the page
 if (!pageLocation.includes("index")) {
   document.getElementsByTagName("a")[1].classList.remove("active");
 } else if (!pageLocation.includes("explore")) {
@@ -10,13 +11,19 @@ if (!pageLocation.includes("index")) {
 
 var box = document.getElementById("box");
 
-function rotate() {
+//this code changes the display of text from none to block when the box is clicked
+
+box.addEventListener("click", function() {
+  //the box.classList.toggle changes the class of box from rotate to rotated and vice-versa
   box.classList.toggle("rotate");
   box.classList.toggle("rotated");
+
+  //this subInfo value is temporary just to get the cast, this variable is subject to change
   var subInfo = document.querySelector(".sub-info");
+  //basic if else to change the style of the text from none to block and vice-versa
   if (box.classList.contains("rotated")) {
     subInfo.style.display = "block";
   } else {
     subInfo.style.display = "none";
   }
-}
+});
