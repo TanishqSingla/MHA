8a728fd06cf804a7b51ace8b5cd9ab60d1c9bfb9
TanishqSingla <tanishqsingla00@gmail.com>
2020-02-14 22:45:00 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,24 +1,22 @@
 var pageLocation = window.location.href;
 
 if (!pageLocation.includes("index")) {
-	document.getElementsByTagName("a")[1].classList.remove("active");
+  document.getElementsByTagName("a")[1].classList.remove("active");
 } else if (!pageLocation.includes("explore")) {
-	document.getElementsByTagName("a")[2].classList.remove("active");
+  document.getElementsByTagName("a")[2].classList.remove("active");
 } else if (!pageLocation.includes("community")) {
-	document.getElementsByTagName("a")[3].classList.remove("active");
+  document.getElementsByTagName("a")[3].classList.remove("active");
 }
 
 var box = document.getElementById("box");
 
 function rotate() {
-	box.classList.toggle("rotate");
-	box.classList.toggle("rotated");
-	var subInfo = document.querySelector(".sub-info");
-	if (box.classList.contains("rotated")) {
-		subInfo.classList.add("show");
-		subInfo.classList.remove("hide");
-	} else {
-		subInfo.classList.add("hide");
-		subInfo.classList.remove("show");
-	}
+  box.classList.toggle("rotate");
+  box.classList.toggle("rotated");
+  var subInfo = document.querySelector(".sub-info");
+  if (box.classList.contains("rotated")) {
+    subInfo.style.display = "block";
+  } else {
+    subInfo.style.display = "none";
+  }
 }
